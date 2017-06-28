150af68b1d730159fc90f1f27d3e502650407d46
Milad <milad.metias@germanitlab.com>
2017-06-28 11:38:39 +0200

    Adding ImageHelper to centralize image loading calls

--- app/src/main/java/com/germanitlab/kanonhealth/helpers/ImageHelper.java
+++ app/src/main/java/com/germanitlab/kanonhealth/helpers/ImageHelper.java
@@ -1,10 +1,18 @@
 package com.germanitlab.kanonhealth.helpers;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
 import com.germanitlab.kanonhealth.R;
 
 /**
@@ -22,5 +30,41 @@ public class ImageHelper {
                 .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .skipMemoryCache(true)
                 .into(iv);
+    }
+
+    public static void setBackground(final View v, String imageFullUrl, Context ctx) {
+
+        int width = v.getWidth();
+        int height = v.getHeight();
+
+        if (width <= 0) {
+            width = v.getMeasuredWidth();
+        }
+        if (height <= 0) {
+            height = v.getMeasuredHeight();
+        }
+
+        if (width <= 0 && v.getLayoutParams() != null) {
+            width = v.getLayoutParams().width;
+        }
+        if (height <= 0 && v.getLayoutParams() != null) {
+            height = v.getLayoutParams().height;
+        }
+
+
+        Glide.with(ctx)
+                .load(imageFullUrl)
+                .asBitmap()
+                .into(new SimpleTarget<Bitmap>(width, height) {
+                    @Override
+                    public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
+                        Drawable drawable = new BitmapDrawable(resource);
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                            v.setBackground(drawable);
+                        } else {
+                            v.setBackgroundDrawable(drawable);
+                        }
+                    }
+                });
     }
 }
