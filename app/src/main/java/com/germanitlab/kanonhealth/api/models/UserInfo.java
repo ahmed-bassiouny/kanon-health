175bf67fffb97756263f79f453b3e9117ee86587
norhan <norhanelsawi@germanitlab.com>
2017-08-02 14:59:33 +0200

    user info update

--- app/src/main/java/com/germanitlab/kanonhealth/api/models/UserInfo.java
+++ app/src/main/java/com/germanitlab/kanonhealth/api/models/UserInfo.java
@@ -3,6 +3,7 @@ package com.germanitlab.kanonhealth.api.models;
 import com.google.gson.annotations.SerializedName;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by bassiouny on 02/08/17.
@@ -21,15 +22,25 @@ public class UserInfo extends ParentModel{
     public static final String KEY_GENDER="gender";
     public static final String KEY_BIRTHDAY="birh_day";
     public static final String KEY_AVATAR="avatar";
-    public static final String KEY_DOCTOR="doctor";
+    public static final String KEY_DOCTOR_ID="doctor_id";
     public static final String KEY_CLINICS="clinics";
     public static final String KEY_DOCUMENTS="documents";
+    public static final String KEY_EMAIL =    "email";
+    public static final String KEY_RATE_NUM = "rate_num";
+    public static final String KEY_RATE_PERCENTAGE = "rate_percentage";
+    public static final String KEY_ADDRESS = "address";
+    public static final String KEY_SUPPORTED_LANG = "supported_lang";
+    public static final String KEY_IS_AVAILABLE= "is_available";
+
 
     public static final int DOCTOR=2;
     public static final int PATIENT=1;
 
     @SerializedName(KEY_USERID)
     private Integer userID;
+
+    @SerializedName(KEY_DOCTOR_ID)
+    private Integer doctorID;
     // usertype => 1 patient , 2 doctor
     @SerializedName(KEY_USERTYPE)
     private Integer userType;
@@ -51,6 +62,32 @@ public class UserInfo extends ParentModel{
     private String  birh_day;
     @SerializedName(KEY_AVATAR)
     private String  avatar;
+    @SerializedName(KEY_IS_AVAILABLE)
+    private Integer isAvailable;
+
+    @SerializedName(KEY_DOCUMENTS )
+    private ArrayList<Document> documents;
+
+    @SerializedName(KEY_CLINICS)
+    private ArrayList<Clinic> clinics;
+
+    @SerializedName(KEY_EMAIL)
+    private String email;
+
+    @SerializedName(KEY_RATE_NUM)
+    private Float rateNum;
+
+    @SerializedName(KEY_RATE_PERCENTAGE)
+    private HashMap<String, String> ratePercentage;
+
+    @SerializedName(KEY_ADDRESS)
+    private String address;
+
+    @SerializedName(KEY_SUPPORTED_LANG)
+
+    private ArrayList<SupportedLang> supportedLangs;
+
+
     /*
     // i need some data to make model
     * clinic
@@ -148,5 +185,89 @@ public class UserInfo extends ParentModel{
 
     public void setAvatar(String avatar) {
         this.avatar = avatar;
+    }
+    public Integer getAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(Integer available) {
+        isAvailable = available;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+    public ArrayList<SupportedLang> getSupportedLangs() {
+        return supportedLangs;
+    }
+
+    public void setSupportedLangs(ArrayList<SupportedLang> supportedLangs) {
+        this.supportedLangs = supportedLangs;
+    }
+
+    public static String getKeyUserid() {
+        return KEY_USERID;
+    }
+
+    public Integer getDoctorID() {
+        return doctorID;
+    }
+
+    public void setDoctorID(Integer doctorID) {
+        this.doctorID = doctorID;
+    }
+
+    public Integer getIsAvailable() {
+        return isAvailable;
+    }
+
+    public void setIsAvailable(Integer isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+
+    public ArrayList<Document> getDocuments() {
+        return documents;
+    }
+
+    public void setDocuments(ArrayList<Document> documents) {
+        this.documents = documents;
+    }
+
+    public ArrayList<Clinic> getClinics() {
+        return clinics;
+    }
+
+    public void setClinics(ArrayList<Clinic> clinics) {
+        this.clinics = clinics;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Float getRateNum() {
+        return rateNum;
+    }
+
+    public void setRateNum(Float rateNum) {
+        this.rateNum = rateNum;
+    }
+
+    public HashMap<String, String> getRatePercentage() {
+        return ratePercentage;
+    }
+
+    public void setRatePercentage(HashMap<String, String> ratePercentage) {
+        this.ratePercentage = ratePercentage;
     }
 }
