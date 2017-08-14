4e15424b0cebd48ddfd96114622e48d00e4fb681
Norhan Elsawi <norhanelsawi@germanitlab.com>
2017-08-14 14:53:07 +0200

    preRequest

--- app/src/main/java/com/germanitlab/kanonhealth/payment/PreRequest.java
+++ app/src/main/java/com/germanitlab/kanonhealth/payment/PreRequest.java
@@ -6,10 +6,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
-
 import com.crashlytics.android.Crashlytics;
 import com.germanitlab.kanonhealth.R;
-import com.germanitlab.kanonhealth.db.PrefManager;
+import com.germanitlab.kanonhealth.helpers.PrefHelper;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -42,8 +41,7 @@ public class PreRequest extends AppCompatActivity {
         try {
             Intent intent = new Intent(this, PaymentActivity.class);
             intent.putExtra("doctor_data", doctorJson);
-            PrefManager prefManager = new PrefManager(this);
-            prefManager.put(prefManager.USER_INTENT,doctorJson);
+            PrefHelper.put(getApplicationContext(),PrefHelper.KEY_USER_INTENT,doctorJson);
             startActivity(intent);
         } catch (Exception e) {
             Crashlytics.logException(e);
