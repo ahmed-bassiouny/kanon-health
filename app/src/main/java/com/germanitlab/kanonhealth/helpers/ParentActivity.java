b69fb2dcd8ea355aff5bce0d72aae23a161e5430
Milad Metias <milad.metias@cleverclan.com>
2017-08-14 15:38:06 +0200

    remove progress static , fixing memory leaked window

--- app/src/main/java/com/germanitlab/kanonhealth/helpers/ParentActivity.java
+++ app/src/main/java/com/germanitlab/kanonhealth/helpers/ParentActivity.java
@@ -1,7 +1,12 @@
 package com.germanitlab.kanonhealth.helpers;
 
+import android.content.Context;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
+
+import com.germanitlab.kanonhealth.R;
+
+import dmax.dialog.SpotsDialog;
 
 /**
  * Created by andy on 7/25/17.
@@ -10,5 +15,21 @@ import android.support.v7.app.AppCompatActivity;
 public abstract class ParentActivity extends AppCompatActivity {
 
     public abstract void ImagePickerCallBack(Uri uri);
+
+    private SpotsDialog currentProgress = null;
+
+    public void showProgressBar() {
+        if (currentProgress == null) {
+            currentProgress = new SpotsDialog(ParentActivity.this, R.style.CustomProgressDialog);
+            currentProgress.show();
+        }
+    }
+
+    public void hideProgressBar() {
+        if (currentProgress != null) {
+            currentProgress.dismiss();
+            currentProgress = null;
+        }
+    }
 
 }
