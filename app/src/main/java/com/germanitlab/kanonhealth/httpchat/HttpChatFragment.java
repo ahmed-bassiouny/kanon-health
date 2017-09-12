4ed6404114ef1f3a57b91a97f726a2d4f53425a1
Noura\Nourhan <nourhanhamada17@gmail.com>
2017-09-12 10:04:42 +0200

    commit try

--- app/src/main/java/com/germanitlab/kanonhealth/httpchat/HttpChatFragment.java
+++ app/src/main/java/com/germanitlab/kanonhealth/httpchat/HttpChatFragment.java
@@ -220,28 +220,28 @@ public class HttpChatFragment extends ParentFragment implements Serializable, Ho
                 getActivity().finish();
             }
         });
-        view.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-            @Override
-            public void onGlobalLayout() {
-                Rect r = new Rect();
-                view.getWindowVisibleDisplayFrame(r);
-                if (view.getRootView().getHeight() - (r.bottom - r.top) > 200) { // if more than 100 pixels, its probably a keyboard...
-                    img_send_txt.setVisibility(View.VISIBLE);
-                      mHoldingButtonLayout.removeListener(fragment);
-
-                } else {
-                    if (etMessage.getText().toString().trim().length() > 0) {
-                        img_send_txt.setVisibility(View.VISIBLE);
-                        mHoldingButtonLayout.removeListener(fragment);
-
-                    } else {
-                        img_send_txt.setVisibility(View.GONE);
-                        mHoldingButtonLayout.addListener(fragment);
-                    }
-
-                }
-            }
-        });
+//        view.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+//            @Override
+//            public void onGlobalLayout() {
+//                Rect r = new Rect();
+//                view.getWindowVisibleDisplayFrame(r);
+//                if (view.getRootView().getHeight() - (r.bottom - r.top) > 200) { // if more than 100 pixels, its probably a keyboard...
+//                    img_send_txt.setVisibility(View.VISIBLE);
+//                      mHoldingButtonLayout.removeListener(fragment);
+//
+//                } else {
+//                    if (etMessage.getText().toString().trim().length() > 0) {
+//                        img_send_txt.setVisibility(View.VISIBLE);
+//                        mHoldingButtonLayout.removeListener(fragment);
+//
+//                    } else {
+//                        img_send_txt.setVisibility(View.GONE);
+//                        mHoldingButtonLayout.addListener(fragment);
+//                    }
+//
+//                }
+//            }
+//        });
         return view;
     }
 
